38c5909669048b6f94dec2819b9018bd020d9201
xec-cm <fcatala@irsicaixa.es>
2021-02-12 14:00:09 +0100

    ca spain map to external data to speed up

--- exec/package_data.R
+++ exec/package_data.R
@@ -1,2 +1,5 @@
 catalunya_map <- rgdal::readOGR("www/comarques-compressed.geojson")
+ca_spain_gj <- rgdal::readOGR("data/georef-spain-comunidad-autonoma.geojson")
+
+usethis::use_data(ca_spain_gj, overwrite = TRUE, compress = "xz")
 usethis::use_data(catalunya_map, overwrite = TRUE, compress = "xz")
