804f82dbbb78d8e86f16141f385e33f1952e47e8
xec-cm <fcatala@irsicaixa.es>
2021-03-09 12:26:57 +0100

    Update module_acknowledgements.R

--- R/module_acknowledgements.R
+++ R/module_acknowledgements.R
@@ -16,21 +16,22 @@ acknowledgements_module_ui <- function(id) {
             ),
             shiny::fixedRow(
                 shiny::h5(
-                    "We gratefully acknowledge all data contributors, i.e.
-                    the Authors and their Originating laboratories responsible
-                    for obtaining the specimens, and their Submitting laboratories
-                    for generating the genetic sequence and metadata and sharing
-                    via the GISAID Initiative (1), on which this research is based.",
+                    "We are grateful to the data contributors who shared the data
+                    used in this Web Application via the GISAID Initia-tive*: the
+                    Authors, the Originating Laboratories responsible for
+                    obtaining the specimens, and the Submitting Laboratories that
+                    generated the genetic sequences and metadata.",
                     align = "center",
                     style = "margin-left: 150px; margin-right: 150px; line-height: 25px;"
                 )
             ),
             shiny::fixedRow(
                 shiny::h5(
-                    "(1) Elbe, S., and Buckland-Merrett, G. (2017) Data, disease
-                    and diplomacy: GISAID’s innovative contribution to global
-                    health. Global Challenges, 1:33-46.
-                    DOI: 10.1002/gch2.1018PMCID: 31565258",
+                    "* (a) Elbe, S., and Buckland-Merrett, G. (2017) Data, disease
+                    and diplomacy: GISAID’s innovative contribution to global health.
+                    Global Challenges, 1:33-46. DOI: 10.1002/gch2.1018 and / or
+                    (b) Shu, Y., McCauley, J. (2017) GISAID: From vision to reality.
+                    EuroSurveillance, 22(13) DOI: 10.2807/1560-7917.ES.2017.22.13.30494.",
                     align = "center",
                     style = "margin-left: 150px; margin-right: 150px; line-height: 25px;"
                 )
