c17c8246013d1124881e43256b99ba096272e122
xec-cm <fcatala@irsicaixa.es>
2021-03-22 13:39:42 +0100

    Update app.R

--- app.R
+++ app.R
@@ -2,8 +2,8 @@ library(covidapp)
 library(ggplot2)
 
 
-# Preporcessing map data --------------------------------------------------
-if (file.exists("data/map_data.rds")) {
+# Preprocessing ap data --------------------------------------------------
+if (!file.exists("data/map_data.rds")) {
     df_map <<- readr::read_rds("data/MergedData_spain.rds") %>%
         map_data(my_map_data = ca_spain_gj, ca_inhabitants = ca_inhabitants)
 
