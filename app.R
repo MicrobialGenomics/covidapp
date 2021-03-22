cc2a6ce8a5b86bedd0023838e590c309957a5be0
xec-cm <fcatala@irsicaixa.es>
2021-03-22 12:58:28 +0100

    Update app.R

--- app.R
+++ app.R
@@ -3,8 +3,14 @@ library(ggplot2)
 
 
 # Preporcessing map data --------------------------------------------------
-df_map <<- readr::read_rds("data/MergedData_spain.rds") %>%
-    map_data(my_map_data = ca_spain_gj, ca_inhabitants = ca_inhabitants)
+if (file.exists("data/map_data.rds")) {
+    df_map <<- readr::read_rds("data/MergedData_spain.rds") %>%
+        map_data(my_map_data = ca_spain_gj, ca_inhabitants = ca_inhabitants)
+
+    readr::write_rds(df_map, file = "data/map_data.rds")
+} else {
+    df_map <<-  readr::read_rds("data/map_data.rds")
+}
 
 shiny::shinyApp(ui = covidapp::ui(), server = covidapp::server())
 
