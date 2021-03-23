4d7747b9a3ae9b48ce313abf782fdca2694da365
xec-cm <fcatala@irsicaixa.es>
2021-03-23 14:03:42 +0100

    update

--- exec/preprocessing_data.R
+++
@@ -1,6 +0,0 @@
-library(covidapp)
-
-df_map <- readr::read_rds("data/MergedData_spain.rds") %>%
-    map_data(my_map_data = ca_spain_gj, ca_inhabitants = ca_inhabitants)
-
-readr::write_rds(df_map, file = "data/map_data.rds")
--- exec/preprocessing_data.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-Rscript /srv/shiny-server/exec/preprocessing_data.R
