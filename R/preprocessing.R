db300522047899100fb9ad1c69127e065cbb3ce4
xec-cm <fcatala@irsicaixa.es>
2021-03-23 09:41:19 +0100

    update

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -25,24 +25,7 @@ base_map <- function(df, map) {
     bins_norm = c(seq(0, max(map$norm_cases) + max(map$norm_cases)*0.2 , by = round(max(map$norm_cases)*0.2)))
     cv_pal_norm <- leaflet::colorBin("Reds", domain = map$norm_cases, bins = bins_norm)
 
-    base_map <- leaflet::leaflet(map) %>%
-        leaflet::addTiles() %>%
-        leaflet::addProviderTiles(leaflet::providers$CartoDB.Positron) %>%
-        leaflet::setView(lng = -4, lat = 40, zoom = 6) %>%
-        leaflet::addLegend(
-            position = "topright",
-            pal = cv_pal_norm,
-            values = ~ norm_cases,
-            title = "<small>Seq. cases per 1e5 inhab.</small>"
-        ) %>%
-        leaflet::addLegend(
-            position = "topright",
-            pal = cv_pal,
-            values = ~ cases,
-            title = "<small>Total Sequenced cases</small>"
-        )
-
-    list(base_map = base_map, cv_pal_norm = cv_pal_norm, cv_pal = cv_pal)
+    list(base_map = map, cv_pal_norm = cv_pal_norm, cv_pal = cv_pal)
 }
 
 #' Title
@@ -71,6 +54,8 @@ map_data <- function(df, my_map_data, ca_inhabitants) {
         )
 
     map <- my_map_data
+    bs_map = base_map(df, my_map_data)
+
     map_plot <- dat$date %>%
         unique() %>%
         purrr::set_names() %>%
@@ -90,7 +75,8 @@ map_data <- function(df, my_map_data, ca_inhabitants) {
                     popup
                 })
 
-            map$cases <- dat %>%
+            res <- list()
+            res$cases <- dat %>%
                 dplyr::count(acom_name, .drop = FALSE) %>%
                 dplyr::left_join(
                     x = tibble::tibble(acom_name = map$acom_name),
@@ -99,44 +85,10 @@ map_data <- function(df, my_map_data, ca_inhabitants) {
                 ) %>%
                 dplyr::pull(n)
 
-            map$norm_cases <- map$cases / ca_inhabitants * 1e5
-            map$norm_cases[is.na(map$norm_cases)] <- 0
+            res$norm_cases <- res$cases / ca_inhabitants * 1e5
+            res$norm_cases[is.na(res$norm_cases)] <- 0
 
-            bs_map = base_map(df, my_map_data)
-
-            norm_map <- bs_map$base_map %>%
-                leaflet::addPolygons(
-                    data = map,
-                    stroke = FALSE,
-                    smoothFactor = 0.3,
-                    fillOpacity = 0.6,
-                    fillColor = ~ bs_map$cv_pal_norm(norm_cases)
-                ) %>%
-                leaflet::addPolygons(
-                    data = map,
-                    stroke = FALSE,
-                    fillOpacity = 0,
-                    fillColor = "transparent",
-                    popup = leafpop::popupGraph(popups, width = 500, height = 300)
-                )
-
-            unnorm_map <- bs_map$base_map %>%
-                leaflet::addPolygons(
-                    data = map,
-                    stroke = FALSE,
-                    smoothFactor = 0.3,
-                    fillOpacity = 0.6,
-                    fillColor = ~ bs_map$cv_pal(cases)
-                ) %>%
-                leaflet::addPolygons(
-                    data = map,
-                    stroke = FALSE,
-                    fillOpacity = 0,
-                    fillColor = "transparent",
-                    popup = leafpop::popupGraph(popups, width = 500, height = 300)
-                )
-
-            list(norm_map = norm_map, unnorm_map = unnorm_map)
+            list(map_data = res, popups = popups)
         })
 
     line_plots <- dat$date %>%
@@ -150,6 +102,7 @@ map_data <- function(df, my_map_data, ca_inhabitants) {
 
     list(
         dat = dat,
+        bs_map = bs_map,
         maps = map_plot,
         line_plots = line_plots
     )
