ba81982583e74088e35c152ac427875640a820ed
xec-cm <fcatala@irsicaixa.es>
2021-03-24 16:35:10 +0100

    speed up

--- R/module_overview.R
+++ R/module_overview.R
@@ -116,13 +116,8 @@ overview_module_server <- function(id) {
         # df_over <<- readr::read_rds("data/MergedData_spain.rds") %>%
         #     dplyr::mutate(acom_name = stringr::str_replace_all(acom_name, "Cataluña", "Catalunya")) %>%
         #     dplyr::filter(!acom_name == "Spain")
-
-        df_over <<- df_map$dat %>%
-            dplyr::mutate(acom_name = stringr::str_replace_all(acom_name, "Cataluña", "Catalunya")) %>%
-            dplyr::filter(!acom_name == "Spain")
-
-        mt <- readr::read_rds("data/MutationEmbeddedData.rds")
-        mt_pos <- extract_mutations(mt)
+        # mt <- readr::read_rds("data/MutationEmbeddedData.rds")
+        # mt_pos <- extract_mutations(mt)
 
         output$mutation_positions <- shiny::renderUI({
             shiny::req(input$var_annot == "mutation")
--- app.R
+++ app.R
@@ -1,5 +1,13 @@
 library(covidapp)
 
+df_map <<- readr::read_rds("data/map_data.rds")
+df_over <<- df_map$dat %>%
+    dplyr::mutate(acom_name = stringr::str_replace_all(acom_name, "Cataluña", "Catalunya")) %>%
+    dplyr::filter(!acom_name == "Spain")
+
+mt <<- readr::read_rds("data/MutationEmbeddedData.rds")
+mt_pos <<- extract_mutations(mt)
+
 shiny::shinyApp(ui = covidapp::ui(), server = covidapp::server())
 
 
