c2307f757ab4afd34046dafd2bb624f4c4fb9d51
xec-cm <fcatala@irsicaixa.es>
2021-04-30 12:25:25 +0200

    speed up per ca module

--- app.R
+++ app.R
@@ -14,6 +14,56 @@ mt_pos <<- extract_mutations(mt)
 # Load per C.A module data ------------------------------------------------
 df_ca <<- df_over
 
+com_aut <- df_ca %>%
+    dplyr::pull(acom_name) %>%
+    unique() %>%
+    sort() %>%
+    c("Spain", .)
+
+
+all_plots <- list()
+for (com in com_aut) {
+    all_plots[["counts"]][["pangolin_lineage"]][[com]] <- df_ca %>%
+        prepro_variants(ca = com, var_anno = "pangolin_lineage") %>%
+        plot_vairants(
+            type = "bar",
+            var = "counts",
+            pal_dir = -1,
+            pal = "mg"
+        )
+
+    all_plots[["counts"]][["NCClade"]][[com]] <- df_ca %>%
+        prepro_variants(ca = com, var_anno = "NCClade") %>%
+        plot_vairants(
+            type = "bar",
+            var = "counts",
+            pal_dir = -1,
+            pal = "mg"
+        )
+
+    all_plots[["freq"]][["pangolin_lineage"]][[com]] <- df_ca %>%
+        prepro_variants(ca = com, var_anno = "pangolin_lineage") %>%
+        plot_vairants(
+            type = "bar",
+            var = "freq",
+            pal_dir = -1,
+            pal = "mg"
+        )
+
+    all_plots[["freq"]][["NCClade"]][[com]] <- df_ca %>%
+        prepro_variants(ca = com, var_anno = "NCClade") %>%
+        plot_vairants(
+            type = "bar",
+            var = "freq",
+            pal_dir = -1,
+            pal = "mg"
+        )
+
+    all_plots
+}
+
+all_plots <<- all_plots
+
 # covidTag initialization -------------------------------------------------
 shiny::shinyApp(ui = covidapp::ui(), server = covidapp::server())
 
